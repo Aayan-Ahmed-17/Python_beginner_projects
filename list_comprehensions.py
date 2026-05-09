122a9825e0af53ece544d02a7b2551cae25995fc
Aayan-Ahmed-17 <aayanahmedtejani@gmail.com>
2026-05-09 17:18:42 +0500

    calculate_factorial function done

--- list_comprehensions.py
+++ list_comprehensions.py
@@ -1,24 +1,47 @@
 # # SqStudent Marks Analyzer
-# std_marks = [45, 78, 96, 23, 67]
+std_marks = [45, 78, 96, 23, 67]
 
-# min_marks = 40
+min_marks = 40
 
-# grace_marks = [mark + 5 if mark < 96 else 100 for mark in std_marks]
-# passed_stds = [mark for mark in grace_marks if mark >= min_marks]
-# failed_stds = [mark for mark in grace_marks if mark < min_marks]
+grace_marks = [mark + 5 if mark < 96 else 100 for mark in std_marks]
+passed_stds = [mark for mark in grace_marks if mark >= min_marks]
+failed_stds = [mark for mark in grace_marks if mark < min_marks]
 
-# print(grace_marks, passed_stds, failed_stds, sep="\n")
+print(grace_marks, passed_stds, failed_stds, sep="\n")
 
 
 #Factorial  Finder
-def calculate_factorial():
-    u_num = int(input("Enter a number: "))
-    
-    # if u_num <= 1:
-    #     print("Please Enter number greater than 1")
-        
-    multipliers = [num for num in range(u_num, 0, -1)]
-    print(multipliers)
-    factorial = [num * num for num in multipliers]
-    
-calculate_factorial()
+def calculate_factorial(number):
+    """
+    Calculates factorial of a number using
+    list comprehension and loops.
+    """
+
+    # Handle negative numbers
+    if number < 0:
+        return "Factorial does not exist for negative numbers."
+
+    # Handle 0
+    if number == 0:
+        return 1
+
+    # Generate numbers using list comprehension
+    numbers = [num for num in range(1, number + 1)]
+
+    # Calculate factorial
+    factorial = 1
+
+    for num in numbers:
+        factorial *= num
+
+    return factorial
+
+
+# User Input
+number = int(input("Enter a number: "))
+
+# Function Call
+result = calculate_factorial(number)
+
+# Output
+print(f"Factorial of {number} is: {result}")
